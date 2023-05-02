d2b2950d4c7224b1384a5d73508b8e05c3ef7cb0
tharunkumar7097 <119399188+tharunkumar7097@users.noreply.github.com>
2023-05-02 22:12:46 +0530

    Add files via upload

---
+++ query_1.sql
@@ -0,0 +1,26 @@
+-- create database and its name.
+-- create database rgm;
+
+-- create table student( 
+-- student_id int primary key not null,
+-- student_name varchar(30) not null,
+-- student_contact int not null,
+-- student_dept varchar(20) not null
+-- );
+-- select *from student;
+
+-- INSERT INTO student(student_id,student_name,student_contact,student_dept)
+-- VALUES(101,"Tharun Kumar",927387483,"CSE(DS)");
+
+-- INSERT INTO student VALUES(102,"Royal",834467365,"CSE");
+-- select *from student;
+INSERT INTO student VALUES(103,"Sam",834467365,"CSE(BS)");
+INSERT INTO student VALUES(104,"Thejesh",99834467,"EEE");
+INSERT INTO student VALUES(105,"Nanda",654467365,"Mech");
+INSERT INTO student VALUES(106,"Charan",88844673,"Civil");
+
+INSERT INTO student VALUES
+(107,"kumar",674467365,"ECE"),(108,"Reddy",998667365,"CSE"),(109,"Ram",834467376,"AI"),
+(110,"swamy",764467365,"CSE"),(111,"koyal",876767365,"CSE"),(112,"Rakshit",83467765,"CS"),
+(113,"Ravi",834499365,"IT"),(114,"Rohi",834460005,"CSE"),(115,"Rahul",834467365,"CSE"),
+(116,"Ramu",83446799,"CSE_CS");
