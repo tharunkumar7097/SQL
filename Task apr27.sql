39ac3afb53252ebb4739a118266382140e650d88
tharunkumar7097 <119399188+tharunkumar7097@users.noreply.github.com>
2023-05-02 22:14:04 +0530

    27 apr

---
+++ Task apr27.sql
@@ -0,0 +1,13 @@
+create database Herovired;
+
+CREATE TABLE rgm(name varchar(20)  not null,
+college varchar(100) not null,
+session varchar(40)  not null,
+contact_Number int primary key  not null, 
+Address varchar(50) not null);
+ INSERT INTO rgm VALUES(201,"Tharun ","RGM","Python",834467365,"ATP"),
+ (202,"kumar","RGM","SQL",674467365,"KNL"),(203,"Reddy","SKU","Python",998667365,"Chittor"),
+ (204,"Ram","santhiram","c#",834467376,"AI"),(205,"swamy","SRM","python",764467365,"kadapa"),
+ (206,"koyal","RGM","HTML",876767365,"NDL"),(207,"Rakshit","JNTU","SQL",83467765,"KDK"),
+("208,Ravi","CMR",".NET",834499365,"Hyderabad"),(209,"Rohi","SVR","C",834460005,"NDL"),
+(210,"Rahul","JNV","node JS",83446736,"LPK");
